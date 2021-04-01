c135b3a04d9b4c373f8406ca9fe46ca160f6d352
Valery Pogorelov <val.pogorelov@gmail.com>
2021-04-01 11:39:53 +0300

    DispatcherWrapper added.

---
+++ Ssz.Utils.Wpf/DispatcherWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Ssz.Utils.Wpf
+{   
+    public class DispatcherWrapper : IDispatcher
+    {
+        #region construction and destruction
+
+        public DispatcherWrapper(Dispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        #endregion
+
+        #region public functions
+
+        public void BeginInvoke(Action<CancellationToken> action)
+        {
+            _dispatcher.BeginInvoke(action);
+        }
+
+        #endregion
+
+        #region private fields
+
+        private Dispatcher _dispatcher;
+
+        #endregion
+    }
+}
