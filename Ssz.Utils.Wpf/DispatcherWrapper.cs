b48c873c868795bee78a446af79ffd27dbb24356
Valery Pogorelov <val.pogorelov@gmail.com>
2021-04-01 15:09:27 +0300

    Dispatcher dev.

--- Ssz.Utils.Wpf/DispatcherWrapper.cs
+++ Ssz.Utils.Wpf/DispatcherWrapper.cs
@@ -8,7 +8,7 @@ using System.Windows.Threading;
 
 namespace Ssz.Utils.Wpf
 {   
-    public class DispatcherWrapper : IDispatcher
+    public class DispatcherWrapper : IDispatcher, IDisposable
     {
         #region construction and destruction
 
@@ -17,20 +17,58 @@ namespace Ssz.Utils.Wpf
             _dispatcher = dispatcher;
         }
 
+        /// <summary>
+        ///     This is the implementation of the IDisposable.Dispose method.  The client
+        ///     application should invoke this method when this instance is no longer needed.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     This method is invoked when the IDisposable.Dispose or Finalize actions are
+        ///     requested.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _cancellationTokenSource.Cancel();
+            }
+
+            Disposed = true;
+        }
+
+        /// <summary>
+        ///     Invoked by the .NET Framework while doing heap managment (Finalize).
+        /// </summary>
+        ~DispatcherWrapper()
+        {
+            Dispose(false);
+        }
+
         #endregion
 
         #region public functions
 
+        public bool Disposed { get; private set; }
+
         public void BeginInvoke(Action<CancellationToken> action)
         {
-            _dispatcher.BeginInvoke(action);
-        }
+            if (Disposed) return;
+
+            _dispatcher.BeginInvoke(action, _cancellationTokenSource.Token);
+        }        
 
         #endregion
 
         #region private fields
 
         private Dispatcher _dispatcher;
+
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
         #endregion
     }
