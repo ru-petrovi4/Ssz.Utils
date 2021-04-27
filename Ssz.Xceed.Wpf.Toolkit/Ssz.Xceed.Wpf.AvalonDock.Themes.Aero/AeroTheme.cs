28a47cda7673775667df332627c6671f9e0bd926
Valery Pogorelov <val.pogorelov@gmail.com>
2021-04-27 14:57:41 +0300

    Ssz.Xceed.Wpf.Toolkit added.

---
+++ Ssz.Xceed.Wpf.Toolkit/Ssz.Xceed.Wpf.AvalonDock.Themes.Aero/AeroTheme.cs
@@ -0,0 +1,33 @@
+/*************************************************************************************
+   
+   Toolkit for WPF
+
+   Copyright (C) 2007-2018 Ssz.Xceed Software Inc.
+
+   This program is provided to you under the terms of the Microsoft Public
+   License (Ms-PL) as published at http://wpftoolkit.codeplex.com/license 
+
+   For more features, controls, and fast professional support,
+   pick up the Plus Edition at https://xceed.com/xceed-toolkit-plus-for-wpf/
+
+   Stay informed: follow @datagrid on Twitter or Like http://facebook.com/datagrids
+
+  ***********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ssz.Xceed.Wpf.AvalonDock.Themes
+{
+    public class AeroTheme : Theme
+    {
+        public override Uri GetResourceUri()
+        {
+            return new Uri(
+                "/Ssz.Xceed.Wpf.AvalonDock.Themes.Aero;component/Theme.xaml", 
+                UriKind.Relative);  
+        }
+    }
+}
