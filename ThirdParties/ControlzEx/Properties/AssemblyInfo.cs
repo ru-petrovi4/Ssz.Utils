96b46631d685f71ed33e3e346a701dcdb096ded3
Valery Pogorelov <vpogorelov@simcode.dev>
2022-01-25 21:50:33 +0300

    Fluent ribbon lib working.

--- ThirdParties/ControlzEx/Properties/AssemblyInfo.cs
+++ ThirdParties/ControlzEx/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Versioning;
 using System.Windows;
 using System.Windows.Markup;
 
@@ -9,4 +10,11 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("urn:controlzex", "ControlzEx.Theming")]
 [assembly: XmlnsDefinition("urn:controlzex", "ControlzEx.Windows.Shell")]
 
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
--- ThirdParties/Fluent.Ribbon/Properties/AssemblyInfo.cs
+++ ThirdParties/Fluent.Ribbon/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Windows;
 using System.Windows.Markup;
 
 [assembly: XmlnsPrefix("urn:fluent-ribbon", "fluent")]
@@ -8,3 +10,12 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.TemplateSelectors")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Theming")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Metro.Behaviours")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
