f958903d21e673b8a23d60e1013f99fb3644bc2b
Valery Pogorelov <vpogorelov@simcode.dev>
2022-04-11 22:08:21 +0300

    Optimization

---
+++ Ssz.Utils/GuidAndName.cs
@@ -0,0 +1,47 @@
+using System;
+using Ssz.Utils.Serialization;
+
+namespace Ssz.Utils
+{
+    public class GuidAndName : IOwnedDataSerializable
+    {
+        #region public functions
+
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; } = @"";
+
+        public void SerializeOwnedData(SerializationWriter writer, object? context)
+        {
+            writer.Write(Guid);
+            writer.Write(Name);
+        }
+
+        public void DeserializeOwnedData(SerializationReader reader, object? context)
+        {
+            Guid = reader.ReadGuid();
+            Name = reader.ReadString();
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            var other = obj as GuidAndName;
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Guid == other.Guid;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return @"";
+        }
+
+        #endregion
+    }
+}
