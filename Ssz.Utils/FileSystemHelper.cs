570bf9c298c686114ba1237e2cb7fc4f798f7121
Valery Pogorelov <vpogorelov@simcode.dev>
2023-08-10 10:24:58 +0300

    Small API improve.

--- Ssz.Utils/FileSystemHelper.cs
+++ Ssz.Utils/FileSystemHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -214,6 +215,21 @@ namespace Ssz.Utils
             }
         }
 
-#endregion
+        public static void ExecuteScript(string pathToScript)
+        {
+            var scriptArguments = "-ExecutionPolicy Bypass -File \"" + pathToScript + "\"";
+            var processStartInfo = new ProcessStartInfo("powershell.exe", scriptArguments);
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+
+            using var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            Console.WriteLine(output); // I am invoked using ProcessStartInfoClass!
+        }
+
+        #endregion
     }
 }
