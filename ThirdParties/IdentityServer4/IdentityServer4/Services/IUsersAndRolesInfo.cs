5fe52f40aab72fb50697d84aa48ddea80e0041fd
Valery Pogorelov <vpogorelov@simcode.dev>
2023-08-15 14:19:28 +0300

    Interface added.

---
+++ ThirdParties/IdentityServer4/IdentityServer4/Services/IUsersAndRolesInfo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.Services
+{
+    /// <summary>
+    ///     Gets information about users and roles.
+    /// </summary>
+    public interface IUsersAndRolesInfo
+    {
+        /// <summary>
+        ///     Gets all user roles names.
+        /// </summary>
+        /// <returns></returns>
+        string[] GetRoles();
+
+        /// <summary>
+        ///     Gets all user roles names.
+        /// </summary>
+        /// <returns></returns>
+        Task<string[]> GetRolesAsync();
+    }
+}
