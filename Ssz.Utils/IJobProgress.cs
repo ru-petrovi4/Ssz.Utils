72c6e49c45efacdf1813bf966b93afd6e765e7ad
valpogorelov <val.pogorelov@gmail.com>
2023-09-26 20:03:50 +0300

    Progress API improved.

--- Ssz.Utils/IJobProgress.cs
+++ Ssz.Utils/IJobProgress.cs
@@ -9,6 +9,26 @@ namespace Ssz.Utils
         string JobId { get; }
 
         /// <summary>
+        ///     Процент выполнения задачи 0 - 100
+        /// </summary>		
+        uint ProgressPercent { get; }
+
+        /// <summary>
+        ///     Лейбл о статусе выполнения или сообщение об ошибке
+        /// </summary>		
+        string ProgressLabel { get; }
+
+        /// <summary>
+        ///     Детали о статусе исполнение или детали об ошибке
+        /// </summary>		
+        string ProgressDetail { get; }
+
+        /// <summary>
+        ///     See consts in <see cref="JobStatusCodes"/>.        
+        /// </summary>
+        public uint JobStatusCode { get; }
+
+        /// <summary>
         ///     If parameter is null, the parameter does not change.
         /// </summary>
         /// <param name="progressPercent"></param>
@@ -18,7 +38,14 @@ namespace Ssz.Utils
         /// <returns></returns>
         Task SetJobProgressAsync(uint? progressPercent, string? progressLabel, string? progressDetails, uint jobStatusCode);
 
-        IJobProgress GetChildJobProgress(uint minProgressPercent, uint maxProgressPercent);
+        /// <summary>
+        ///     failedIfChildFailed - If child JobProgress failed, then this JobProgress failed. Otherwise, this progress continues.
+        /// </summary>
+        /// <param name="minProgressPercent"></param>
+        /// <param name="maxProgressPercent"></param>
+        /// <param name="failedIfChildFailed"></param>
+        /// <returns></returns>
+        IJobProgress GetChildJobProgress(uint minProgressPercent, uint maxProgressPercent, bool failedIfChildFailed);
     }
 
     public class NullJobProgress : IJobProgress
@@ -26,6 +53,26 @@ namespace Ssz.Utils
         public static readonly NullJobProgress Instance = new();
 
         public string JobId => @"";
+
+        /// <summary>
+        ///     Процент выполнения задачи 0 - 100
+        /// </summary>		
+        public uint ProgressPercent { get; private set; }
+
+        /// <summary>
+        ///     Лейбл о статусе выполнения или сообщение об ошибке
+        /// </summary>		
+        public string ProgressLabel { get; private set; } = @"";
+
+        /// <summary>
+        ///     Детали о статусе исполнение или детали об ошибке
+        /// </summary>		
+        public string ProgressDetail { get; private set; } = @"";
+
+        /// <summary>
+        ///     See consts in <see cref="JobStatusCodes"/>.        
+        /// </summary>
+        public uint JobStatusCode { get; private set; }
 
         /// <summary>
         ///     
@@ -40,7 +87,7 @@ namespace Ssz.Utils
             return Task.CompletedTask;
         }
 
-        public IJobProgress GetChildJobProgress(uint minProgressPercent, uint maxProgressPercent)
+        public IJobProgress GetChildJobProgress(uint minProgressPercent, uint maxProgressPercent, bool failedIfChildFailed)
         {
             return this;
         }
