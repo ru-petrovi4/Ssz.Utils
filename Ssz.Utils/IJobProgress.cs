f9943d96e9402b057fd42a19baca7de0b2ed0599
valpogorelov <val.pogorelov@gmail.com>
2023-11-26 16:24:23 +0300

    Hotfixes.

--- Ssz.Utils/IJobProgress.cs
+++ Ssz.Utils/IJobProgress.cs
@@ -26,7 +26,7 @@ namespace Ssz.Utils
         /// <summary>
         ///     See consts in <see cref="StatusCodes"/>.        
         /// </summary>
-        public uint JobStatusCode { get; }
+        public uint StatusCode { get; }
 
         /// <summary>
         ///     If parameter is null, the parameter does not change.
@@ -72,7 +72,7 @@ namespace Ssz.Utils
         /// <summary>
         ///     See consts in <see cref="StatusCodes"/>.        
         /// </summary>
-        public uint JobStatusCode { get; private set; }
+        public uint StatusCode { get; private set; }
 
         /// <summary>
         ///     
