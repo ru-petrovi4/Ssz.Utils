6d15edbd1d9183382ddf18849c140bca0a24d182
vpogorelov <vpogorelov@cdt.su>
2025-01-13 16:24:09 +0300

    net8.0 -> net9.0

--- Ssz.Dcs/Copy Instructor to FilesStore [Debug].ps1
+++ Ssz.Dcs/Copy Instructor to FilesStore [Debug].ps1
@@ -2,6 +2,6 @@ $PSScriptRoot = ($MyInvocation.MyCommand.Path | Split-Path | Resolve-Path).Provi
 
 Remove-Item "C:\Dcs.CentralServer\FilesStore\Instructor.Bin\*" -Recurse -Force
 
-Copy-Item -Path "$PSScriptRoot\Ssz.Dcs.Instructor\bin\Debug\net8.0-windows\*" -Destination "C:\Dcs.CentralServer\FilesStore\Instructor.Bin" -Recurse
+Copy-Item -Path "$PSScriptRoot\Ssz.Dcs.Instructor\bin\Debug\net9.0-windows\*" -Destination "C:\Dcs.CentralServer\FilesStore\Instructor.Bin" -Recurse
 
 Read-Host -Prompt "Press Enter to exit"
