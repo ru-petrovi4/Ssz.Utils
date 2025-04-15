8793b0abf94bf85ac601ba5495b84805650fefac
Валерий Погорелов <vpogorelov@cdt.su>
2025-04-15 12:49:02 +0300

    Serialization UnknownObject added.

---
+++ Ssz.Utils/Serialization/UnknownObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ssz.Utils.Serialization
+{
+    public class UnknownObject
+    {
+        public string TypeFullName { get; set; } = null!;
+
+        public byte[] Data { get; set; } = null!;
+    }
+}
