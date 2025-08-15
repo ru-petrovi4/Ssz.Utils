85f923867b4a4527a42983da8d08e475e970c0f6
Валерий Погорелов <vpogorelov@cdt.su>
2025-08-15 14:02:01 +0300

    FastList improved.

--- Ssz.Utils/Optimized/FastList.cs
+++ Ssz.Utils/Optimized/FastList.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Ssz.Utils;
 
-public class FastList<T>
+public class FastList<T> : IList<T>, IReadOnlyList<T>
 {
     #region construction and destruction
 
@@ -22,12 +23,15 @@ public class FastList<T>
 
     public int Count => _count;
 
+    public bool IsReadOnly => false;
+
     public void Add(T item)
     {
         if (_count >= _items.Length)
             Resize(_items.Length * 2);
 
-        _items[_count++] = item;
+        _items[_count] = item;
+        _count += 1;
     }
 
     public void AddRange(ReadOnlySpan<T> span)
@@ -73,7 +77,41 @@ public class FastList<T>
         _items = newArray;
     }
 
-    #endregion    
+    public int IndexOf(T item)
+    {
+        return Array.IndexOf(_items, item, 0, _count);
+    }
+
+    public void Insert(int index, T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        return Array.IndexOf(_items, item, 0, _count) > -1;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        Array.Copy(_items, 0, array, arrayIndex, _count);
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator() => new Enumerator(this);    
+
+    IEnumerator IEnumerable.GetEnumerator() => new Enumerator(this);
+
+    #endregion
 
     #region private fields
 
@@ -84,4 +122,54 @@ public class FastList<T>
     private int _count;
 
     #endregion
+
+    public struct Enumerator : IEnumerator<T>, IEnumerator
+    {
+        internal static IEnumerator<T>? s_emptyEnumerator;
+
+        private readonly FastList<T> _list;
+        private int _index;        
+        private T? _current;
+
+        internal Enumerator(FastList<T> list)
+        {
+            _list = list;
+            _index = 0;            
+            _current = default;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            FastList<T> localList = _list;
+
+            if ((uint)_index < (uint)localList._count)
+            {
+                _current = localList._items[_index];
+                _index++;
+                return true;
+            }
+            return MoveNextRare();
+        }
+
+        private bool MoveNextRare()
+        {
+            _index = _list._count + 1;
+            _current = default;
+            return false;
+        }
+
+        public T Current => _current!;
+
+        object? IEnumerator.Current => _current;
+        
+        void IEnumerator.Reset()
+        {   
+            _index = 0;
+            _current = default;
+        }
+    }
 }
