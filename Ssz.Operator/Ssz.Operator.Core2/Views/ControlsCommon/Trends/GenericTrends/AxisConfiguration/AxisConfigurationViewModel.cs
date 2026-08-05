2d2d2a3e5dda07cd3e933d03216f61341f8fec6c
Valery Pogorelov <val.pogorelov@gmail.com>
2026-08-05 17:31:55 +0300

    Packages update.

--- Ssz.Operator/Ssz.Operator.Core2/Views/ControlsCommon/Trends/GenericTrends/AxisConfiguration/AxisConfigurationViewModel.cs
+++ Ssz.Operator/Ssz.Operator.Core2/Views/ControlsCommon/Trends/GenericTrends/AxisConfiguration/AxisConfigurationViewModel.cs
@@ -1,39 +1,38 @@
 ﻿using Ssz.Utils;
 
-namespace Ssz.Operator.Core.ControlsCommon.Trends.GenericTrends.AxisConfiguration
+namespace Ssz.Operator.Core.ControlsCommon.Trends.GenericTrends.AxisConfiguration;
+
+public class AxisConfigurationViewModel : ViewModelBase
 {
-    public class AxisConfigurationViewModel : ViewModelBase
+    #region construction and destruction
+
+    public AxisConfigurationViewModel(string variableId, double minimum, double maximum)
     {
-        #region construction and destruction
-
-        public AxisConfigurationViewModel(string variableId, double minimum, double maximum)
-        {
-            Title = string.Format("Установить Шкалу Y для {0}", variableId);
-            Minimum = minimum;
-            Maximum = maximum;
-        }
-
-        #endregion
-
-        #region public functions
-
-        public string Title { get; private set; }
-
-        public double Minimum { get; set; }
-        public double Maximum { get; set; }
-
-        #endregion
+        Title = string.Format("Установить Шкалу Y для {0}", variableId);
+        Minimum = minimum;
+        Maximum = maximum;
     }
 
-    public class AxisConfigurationDesignData : AxisConfigurationViewModel
+    #endregion
+
+    #region public functions
+
+    public string Title { get; private set; }
+
+    public double Minimum { get; set; }
+    public double Maximum { get; set; }
+
+    #endregion
+}
+
+public class AxisConfigurationDesignData : AxisConfigurationViewModel
+{
+    #region construction and destruction
+
+    public AxisConfigurationDesignData() :
+        base("TCA2008/PID1/PV/CV", -50, 150)
     {
-        #region construction and destruction
-
-        public AxisConfigurationDesignData() :
-            base("TCA2008/PID1/PV/CV", -50, 150)
-        {
-        }
-
-        #endregion
     }
+
+    #endregion
 }
